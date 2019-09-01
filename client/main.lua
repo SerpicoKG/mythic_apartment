990cf6c4ff98386177a2c1feb128d498f9547ecb
Alzar <srichardson97402@hotmail.com>
2019-09-01 07:34:47 -0700

    Update to new Print3DText Function

--- client/main.lua
+++ client/main.lua
@@ -3,25 +3,29 @@ local inApartment = false
 local isLoggedIn = false
 
 function Print3DText(coords, text)
-	local onScreen, _x, _y = World3dToScreen2d(coords.x, coords.y, coords.z)
+    local onScreen, _x, _y = World3dToScreen2d(coords.x, coords.y, coords.z)
 
-	if onScreen then
-		SetTextScale(0.35, 0.35)
-		SetTextFont(4)
-		SetTextProportional(1)
-		SetTextColour(255, 255, 255, 255)
-		SetTextDropShadow(0, 0, 0, 55)
-		SetTextEdge(0, 0, 0, 150)
-		SetTextDropShadow()
-		SetTextOutline()
-		SetTextEntry("STRING")
-		SetTextCentre(1)
-		AddTextComponentString(text)
-		DrawText(_x,_y)
-
-		local factor = (string.len(text)) / 370
-		DrawRect(_x,_y + 0.0125, 0.015 + factor, 0.03, 255, 0, 0, 68)
-	end
+    if onScreen then
+        local px, py, pz = table.unpack(GetGameplayCamCoords())
+        local dist = #(vector3(px, py, pz) - vector3(coords.x, coords.y, coords.z))    
+        local scale = (1 / dist) * 20
+        local fov = (1 / GetGameplayCamFov()) * 100
+        local scale = scale * fov   
+        SetTextScale(0.35, 0.35)
+        SetTextFont(4)
+        SetTextProportional(1)
+        SetTextColour(250, 250, 250, 255)		-- You can change the text color here
+        SetTextDropshadow(1, 1, 1, 1, 255)
+        SetTextEdge(2, 0, 0, 0, 150)
+        SetTextDropShadow()
+        SetTextOutline()
+        SetTextEntry("STRING")
+        SetTextCentre(1)
+        AddTextComponentString(text)
+        SetDrawOrigin(coords.x, coords.y, coords.z, 0)
+        DrawText(0.0, 0.0)
+        ClearDrawOrigin()
+    end
 end
 
 
