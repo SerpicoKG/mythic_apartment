6481e2750208a24842b0188260c358bc2c1baa84
iAlzar <srichardson97402@hotmail.com>
2019-10-13 18:59:54 -0700

    Change to new inventory structure

--- server/main.lua
+++ server/main.lua
@@ -3,6 +3,6 @@ AddEventHandler('mythic_apartment:server:GetStash', function()
     local src = source
 	local mPlayer = exports['mythic_base']:FetchComponent('Fetch'):Source(src)
     local cData = mPlayer:GetData('character'):GetData()
-    local owner = 'stash-' .. cData.id
+	local owner = { type = 8, owner = cData.id }
     TriggerEvent('mythic_inventory:server:GetSecondaryInventory', src, owner)
 end)
